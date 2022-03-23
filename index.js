7fa356f1c75e271d8593254bee4a15688634b13b
Pinguim334 <88704039+Mooniemd@users.noreply.github.com>
2022-03-23 14:59:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+class Noticia{
+    constructor(titulo, dataPubli, resumo, texto){
+    this.titulo = titulo;
+    this.dataPubli = dataPubli;
+    this.resumo = resumo;
+    this.texto = texto;
+}
+
+divulgarNoticia(){
+    console.log(this.titulo);
+    console.log(this.dataPubli);
+    console.log(this.resumo);
+    console.log(this.texto);
+}
+}
+
+let noti = new Noticia("Atenção atenção, você caiu no pão!", "28/12/2005", "ele caiu no pão.", "top10textosdosanimes");
+noti.divulgarNoticia();
